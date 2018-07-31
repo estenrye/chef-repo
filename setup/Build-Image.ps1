12e7230c52891e0a9185645c4600a29105736d75
Esten Rye <esten@ryezone.net>
2018-07-30 22:35:19 -0500

    Add logic for loading box into vagrant.

--- setup/Build-Image.ps1
+++ setup/Build-Image.ps1
@@ -1,3 +1,5 @@
 Push-Location "$PSScriptRoot/../packer"
 ../bin/packer.exe build hyperv-ubuntu-16.04.json
+$hash =  Get-FileHash -Algorithm sha256 .\hyperv_ubuntu-16.04_chef.box
+vagrant.exe box add --checksum $hash.Hash --checksum-type $hash.Algorithm.ToLower() --provider hyperv --name 'estenrye/ubuntu-16.04' --force $hash.Path
 Pop-Location
