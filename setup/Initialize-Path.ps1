c656b501ecf957d5fc518665fe5165d0f4f703bf
Esten Rye <esten.rye@ryezone.com>
2018-09-03 20:51:34 -0700

    remove need to execute path init as asmin.

--- setup/Initialize-Path.ps1
+++ setup/Initialize-Path.ps1
@@ -1,8 +1,7 @@
-# Run this as an administrator.
 # From http://www.hurryupandwait.io/blog/need-an-ssh-client-on-windows-dont-use-putty-or-cygwinuse-git
 if (-not $env:PATH.Contains('C:/Program Files/Git/usr/bin'))
 {
     $new_path = "$env:PATH;C:/Program Files/Git/usr/bin"
     $env:PATH=$new_path
-    [Environment]::SetEnvironmentVariable("path", $new_path, "Machine")
+    [Environment]::SetEnvironmentVariable("path", $new_path, "user")
 }
